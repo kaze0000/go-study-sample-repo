cef2da8f93da1cdda151d766bd8b9b89e609a68e
kaze0000 <kazuya.00000.ds25@gmail.com>
2023-04-02 18:18:40 +0900

    cmd/server/main.goにコメント追加

--- grpc/cmd/server/main.go
+++ grpc/cmd/server/main.go
@@ -15,18 +15,20 @@ import (
 )
 
 type myServer struct {
+	// hello_grpc.pb.goに以下のようにある
+	// UnimplementedGreetingServiceServer must be embedded to have forward compatible implementations.
 	hellopb.UnimplementedGreetingServiceServer
 }
 
 func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
 	return &hellopb.HelloResponse{
-		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+		Message: fmt.Sprintf("Hello, %s!", req.GetName()), //GetNameもhello_grpc.pb.goに書かれている
 	}, nil
 }
 
-//  自作サービス構造体のコンストラクタを定義
+//  自作サービス構造体のコンストラクタを定義(コンストラクタ関数の名前は、Newで始まる)
 func NewMyServer() *myServer {
-	return &myServer{}
+	return &myServer{} //{}は中身が空のばあいフィールドがゼロ値になる。つまり、初期化している
 }
 
 func main() {
@@ -40,9 +42,13 @@ func main() {
 	s := grpc.NewServer()
 
 	// gRPCサーバーにGreetingServiceを登録
+	// ↓hellopb.RegisterGreetingServiceServer(s, [サーバーに登録するサービス])
 	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
+	// ↑の第2引数には、GreetingServiceServer interface(hello_grpc.pb.goにある)を実装した構造体が入る
+	// ので、第二引数に代入できる自作構造体を定義し、そこでHelloメソッドを実装する。それが上記のmyServer
 
-	reflection.Register(s)
+	reflection.Register(s) //gRPCurlを使うためには、リクエストを送るgRPCサーバーに「サーバーリフレクション」という設定がなされていることが前提となるので、ここで設定
+												 //gRPCulrは、シリアライズのルール知らないので、gRPCサーバから取得できるようにする設定
 
 	// 作成したgRPCサーバーを、8080番ポートで稼働させる
 	go func() {
