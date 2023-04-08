f7a1f55452f003eb06468871939ba990641084b6
kaze0000 <kazuya.00000.ds25@gmail.com>
2023-04-08 20:54:04 +0900

    add func

--- echo-rest-clean/main.go
+++ echo-rest-clean/main.go
@@ -6,11 +6,33 @@ import (
 	"github.com/labstack/echo"
 )
 
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+
+type Message struct {
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Message string `json:"message"`
+}
+
+type Respose struct {
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Message string `json:"message"`
+	Status  string    `json:"status"`
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/users/:name", getUserName) // http://localhost:1323/users/xxx
 	e.GET("/show", show) // http://localhost:1323/show?team=example&member=sasanori
 	e.POST("/save", save)
+
+	e.POST("/users", saveUser) //jsonで返す
+	e.POST("/send", sendMessage) //jsonで受け取って、jsonで返す
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
@@ -30,4 +52,25 @@ func save(c echo.Context) error {
 	email := c.FormValue("email")
 	return c.String(200, fmt.Sprintf("name: %s, email: %s", name, email))
 	// http --form POST http://localhost:1323/save name='kazuya' email='aa@aa.com'
+}
+
+func saveUser(c echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	return c.JSON(200, u)
+}
+
+func sendMessage(c echo.Context) error {
+	m := new(Message)
+	if err := c.Bind(m); err != nil {
+		return err
+	}
+	r := new(Respose)
+	r.Name = m.Name
+	r.Email = m.Email
+	r.Message = m.Message
+	r.Status = "success"
+	return c.JSON(200, r)
 }
