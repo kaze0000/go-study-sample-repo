f3fef91d433c7a3184cfed01f81297d1b3dd6188
kaze0000 <kazuya.00000.ds25@gmail.com>
2023-04-08 17:59:09 +0900

    環境変数を設定する

--- mysql/database/connect.go
+++ mysql/database/connect.go
@@ -3,16 +3,20 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func Connect() *sql.DB{
-	user := "webuser"
-	password := "webpass"
-	host := "localhost"
-	port := "3306"
-	database_name := "go_mysql8_development"
+	err := godotenv.Load() //こいつがいる！！
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST") //dbではなく、localhost
+	port := os.Getenv("DB_PORT")
+	database_name := os.Getenv("DB_DATABASE_NAME")
 
 	dbconf := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + database_name + "?charset=utf8mb4"
 	db, err := sql.Open("mysql", dbconf)
--- mysql/go.mod
+++ mysql/go.mod
@@ -2,4 +2,7 @@ module github.com/kaze0000/go-study-youtube/mysql
 
 go 1.20
 
-require github.com/go-sql-driver/mysql v1.7.0
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/joho/godotenv v1.5.1
+)
