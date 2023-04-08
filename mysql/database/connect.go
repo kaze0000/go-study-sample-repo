237d5b29213134dd423cb17382c9e526a78a85e6
kaze0000 <kazuya.00000.ds25@gmail.com>
2023-04-08 17:50:40 +0900

    connectの処理を分ける

---
+++ mysql/database/connect.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Connect() *sql.DB{
+	user := "webuser"
+	password := "webpass"
+	host := "localhost"
+	port := "3306"
+	database_name := "go_mysql8_development"
+
+	dbconf := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + database_name + "?charset=utf8mb4"
+	db, err := sql.Open("mysql", dbconf)
+	if err != nil {
+			fmt.Println(err.Error())
+	}
+	return db
+}
--- mysql/main.go
+++ mysql/main.go
@@ -2,25 +2,21 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+
+	"github.com/kaze0000/go-study-youtube/mysql/database"
 
 	_ "github.com/go-sql-driver/mysql" // 波線でてるけど、使えている
 )
 
 func main() {
     // [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
-    dbconf := "webuser:webpass@tcp(localhost:3306)/go_mysql8_development?charset=utf8mb4" // データベース接続成功
-    db, err := sql.Open("mysql", dbconf)
+    db := database.Connect()
 
     // 接続が終了したらクローズする
     defer db.Close()
 
-    if err != nil {
-        fmt.Println(err.Error())
-    }
-
-    err = db.Ping()
+    err := db.Ping()
 
     if err != nil {
         fmt.Println("データベース接続失敗")
